0a5ba5b1499916fff9325ca2ad116dd9e746cbe1
Lance Wong <lwong180@gmail.com>
2018-11-04 18:41:04 -0800

    scripts to create db tables and import books data from csv

---
+++ create.py
@@ -0,0 +1,30 @@
+import os 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def create_tables():
+    """Creates books, users, and reviews tables"""
+    commands = (
+        ('CREATE TABLE books ('
+            'isbn VARCHAR PRIMARY KEY, '
+            'title VARCHAR NOT NULL, '
+            'author VARCHAR NOT NULL, '
+            'year SMALLINT NOT NULL)'),
+        ('CREATE TABLE users ('
+            'id SERIAL PRIMARY KEY, '
+            'username VARCHAR NOT NULL) '),
+        ('CREATE TABLE reviews ('
+            'review_id SERIAL PRIMARY KEY, '
+            'review VARCHAR NOT NULL, '
+            'book_id VARCHAR, FOREIGN KEY(book_id) REFERENCES books(isbn), '
+            'user_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(id))')
+        )
+    for command in commands:
+        db.execute(command)
+    db.commit()
+
+if __name__ == '__main__':
+    create_tables()
--- import.py
+++ import.py
@@ -4,35 +4,19 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv('DATABASE_URL'))
 db = scoped_session(sessionmaker(bind=engine))
-
-def create_tables():
-    """Add tables to db"""
-    commands = (
-            """
-            CREATE TABLE books (
-                isbn PRIMARY KEY,
-                title VARCHAR NOT NULL,
-                author VARCHAR NOT NULL,
-                year YEAR(4) NOT NULL
-            )
-            """,
-            """
-            CREATE TABLE users (
-                id SERIAL PRIMARY KEY,
-                username VARCHAR NOT NULL
-            )
-            """,
-            """
-            CREATE TABLE reviews (
-                review_id SERIAL PRIMARY KEY,
-                
-            
-            )
-            """)
 
 def main():
     """import books in PostgreSQL db"""
-    with open('books.csv') as f:
-        reader = csv.reader(f)
+    with open('books.csv', newline='') as f:
+        reader = csv.reader(f, delimiter=',')
+        next(reader)
+        for isbn, title, author, year in reader:
+            db.execute(('INSERT INTO books (isbn, title, author, year)'
+            ' VALUES (:isbn, :title, :author, :year)'),
+            {'isbn': isbn, 'title': title, 'author': author, 'year': int(year)})
+        db.commit()
+
+if __name__ == "__main__":
+    main()
